7d0b478dafaeb2729edfd8be917f8d4e2a9bf728
indra-zen <2450047@student.ppkia.ac.id>
2024-10-16 00:12:16 +0800

    Looping

---
+++ ch.1-introduction/1.3-for-statement/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// print Celsius-Fahrenheit table
+void main() {
+  printf("Celsius-Fahrenheit Table\n");
+  for (int c = 0;c <= 300;c += 20)
+    printf("%3d %6.1f\n", c, (5.0/9.0) * c + 32);
+}
