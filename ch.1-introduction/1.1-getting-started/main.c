395d013b42aeca77458a99c5bd26b5222981d05c
indra-zen <2450047@student.ppkia.ac.id>
2024-10-15 23:23:37 +0800

    I use arch, btw

---
+++ ch.1-introduction/1.1-getting-started/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+  printf("I love Arch!\n");
+}
+
+/* Almost all people use this, but I prefer the above
+int main() {
+  printf("I love Arch!\n");
+  retun 0;
+}
+*/ // These are comments, btw
