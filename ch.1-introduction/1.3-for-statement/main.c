8705df56abc4a8a04dd22eca1a4b7ffeb4f36aeb
indra-zen <2450047@student.ppkia.ac.id>
2024-10-18 21:03:32 +0800

    Reverse loop

--- ch.1-introduction/1.3-for-statement/main.c
+++ ch.1-introduction/1.3-for-statement/main.c
@@ -3,6 +3,6 @@
 // print Celsius-Fahrenheit table
 void main() {
   printf("Celsius-Fahrenheit Table\n");
-  for (int c = 0;c <= 300;c += 20)
+  for (int c = 300;c >= 0;c -= 20)
     printf("%3d %6.1f\n", c, (5.0/9.0) * c + 32);
 }
