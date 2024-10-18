38cf426713f826fbdb01cadee8e20f0ec953cf6d
indra-zen <2450047@student.ppkia.ac.id>
2024-10-18 21:08:21 +0800

    Just wanna show the difference

--- ch.1-introduction/1.3-for-statement/reverse.c
+++ ch.1-introduction/1.3-for-statement/reverse.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-// print Celsius-Fahrenheit table
+// print Celsius-Fahrenheit table in reverse order
 void main() {
   printf("Celsius-Fahrenheit Table\n");
-  for (int c = 0;c <= 300;c += 20)
+  for (int c = 300;c >= 0;c -= 20)
     printf("%3d %6.1f\n", c, (5.0/9.0) * c + 32);
 }
